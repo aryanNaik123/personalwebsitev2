0b1c4183ecf45f0077e7722b262d53d94f66324d
Aryan Naik <aryannnaik@outlook.com>
2023-01-23 13:36:08 -0500

    copywriting

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,9 +6,24 @@ export default function AboutMe() {
       <Link to="/" className="justify-self-start">
         ⏮️
       </Link>
-      <ul className="text-2xl">
-        <li>Did some things at harvard</li>
-        <li>did some more stuff at columbia</li>
+      <ul className="text-xl text-left ml-5">
+        <li>Prev.</li>
+        <li>
+          Summer Associate @{" "}
+          <a className="underline" href="https://www.fouracres.vc/">
+            {" "}
+            Four Acres Capital
+          </a>
+        </li>
+        <li>
+          Blockchain Research @{" "}
+          <a className="underline" href="https://www.joinmassive.com/">
+            Massive
+          </a>
+        </li>
+        <li>Cofounder of VC-backed Crypto startup</li>
+        <li>Research Affiliate @ Harvard</li>
+        <li>Research Assistant @ Columbia</li>
       </ul>
     </div>
   );
