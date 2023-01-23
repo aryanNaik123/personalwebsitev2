55455acbc50765992fd6d8abd0040ef1a254c5c2
Aryan Naik <aryannnaik@outlook.com>
2023-01-23 13:14:22 -0500

    about me - link

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function AboutMe() {
-  return <div>This is all about me</div>;
+  return (
+    <div>
+      <Link to="/" className="justify-self-start">
+        ⏮️
+      </Link>
+      <ul className="text-2xl">
+        <li>Did some things at harvard</li>
+        <li>did some more stuff at columbia</li>
+      </ul>
+    </div>
+  );
 }
