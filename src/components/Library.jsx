dcffa32083f79d90c87189cecc3da9f1faf52c56
Aryan Naik <66840062+aryanNaik123@users.noreply.github.com>
2023-12-06 00:53:08 -0500

    dune quote

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -60,6 +60,10 @@ export default function Library() {
           text="“Get action. Do things; be sane; don't fritter away your time; create, act, take a place wherever you are and be somebody; get action.”"
           author="Theodore Roosevelt"
         />
+        <Quote
+          text="“I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.”"
+          author="Frank Herbert, Dune"
+        />
       </div>
     </div>
   );
