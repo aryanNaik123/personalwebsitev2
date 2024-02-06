8c9b2ebd3abf3cc71720c93c1e322646503ce405
Aryan Naik <66840062+aryanNaik123@users.noreply.github.com>
2024-02-06 11:10:27 -0500

    fixed sizing

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Github from "../images/GitHub-Mark.png";
 export default function Project(props) {
   return (
-    <div className="group flex flex-row items-center border shadow-xl card p-5 mt-5 card-bordered w-max hover:bg-slate-100">
+    <div className="group flex flex-row items-center border shadow-xl card p-5 mt-5 card-bordered w-60 hover:bg-slate-100">
       <p className="card-title text-center group-hover:bg-slate-100">
         {props.projectName}
       </p>
